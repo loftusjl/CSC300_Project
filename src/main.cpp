34f807e040ab3f6b3ded2f143bd23f64395d9926
Jesse Loftus <Jesse.Loftus@gmail.com>
2021-08-19 21:22:01 -0700

    corrected merge conflicts in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     std::cout << "Hello World!!" << std::endl;
-    cout<<"Git commit"<<endl;
-=======
-    std::cout << "Hello World generic!!!" << std::endl;
->>>>>>> 34bdad70eee8670408c083f0d8f4a00f8051c315
+    cout << "Git commit" << endl;
 }
