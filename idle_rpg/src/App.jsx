e9d3a3980be61b2fcbe0b3be787b8a78975c05fd
Jesse Loftus <jesse.loftus@gmail.com>
2021-08-24 09:06:09 -0700

    boilerplate react site prototype

---
+++ idle_rpg/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './App.css'
+import './theme.scss'
+import { SelectMenu } from './SelectMenu.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+      <SelectMenu />
+    </div>
+  )
+}
+
+export default App
---
+++ idle_rpg/src/SelectMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SelectMenu = (props) => {
+	return (
+		<>
+			<h1>SelectMenu</h1>
+			<div className='rpgui-container framed'><p>test</p></div>
+		</>
+	)
+}
