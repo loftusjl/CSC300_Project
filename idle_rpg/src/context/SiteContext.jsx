b641d9cdea69f3506ac1f1246f059826aaeabaa4
Jesse Loftus <Jesse.Loftus@gmail.com>
2021-08-25 20:44:13 -0700

    Created initial setup of stat blocks

--- idle_rpg/src/context/SiteContext.jsx
+++ idle_rpg/src/context/SiteContext.jsx
@@ -2,5 +2,23 @@ import { createContext } from "react";
 
 export const SiteContext = createContext({
 	characterName: '',
+	characterHP: 0,
+	characterAttackBonus: 0,
+	characterEvasionBonus: 0,
+	characterDefenseBonus: 0,
+	enemyName: '',
+	enemyHP: 0,
+	enemyAttackBonus: 0,
+	enemyEvasionBonus: 0,
+	enemyDefenseBonus: 0,
 	setCharacterName: () => { },
+	setCharacterHP: () => { },
+	setCharacterAttackBonus: () => { },
+	setCharacterEvasionBonus: () => { },
+	setCharacterDefenseBonus: () => { },
+	setEnemyName: () => { },
+	setEnemyHP: () => { },
+	setEnemyAttackBonus: () => { },
+	setEnemyEvasionBonus: () => { },
+	setEnemyDefenseBonus: () => { },
 });
--- idle_rpg/src/pages/TravelScreen.jsx
+++ idle_rpg/src/pages/TravelScreen.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const TravelScreen = (props) => {
 	return (
-		<div>TravelScreen</div>
+		<div className='character-stats'>
+			<label>Hit Points:</label>
+			<div data-value="0.4" className='rpgui-progress red' data-rpguitype='progress'>
+				<div className='rpgui-progress-track'>
+					<div className='rpgui-progress-fill red' style={{ width: '40%' }}></div>
+				</div>
+				<div className='rpgui-progress-left-edge'></div>
+				<div className='rpgui-progress-right-edge'></div>
+			</div>
+		</div>
 	)
 }
